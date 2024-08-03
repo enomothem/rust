ef8980f99fad9fb79c0ae7437268e0dac61220f5
enomothem <1192045710@qq.com>
2024-08-03 14:58:23 +0800

    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx

--- rustfile/src/main.rs
+++ rustfile/src/main.rs
@@ -1,10 +1,13 @@
+use std::fs;
+use std::path::Path;
 fn main() {
-    println!("Hello, world!");
-    println!("Git");
-    println!("Hello, Rust!✅");
-    println!("SS");
 
-    println!("Start");
-    // 增加一个注释
-    println!("");
+    let path = Path::new("output");
+
+    if !path.exists() {
+        match fs::create_dir_all(path) {
+            Ok(_) => println!("文件夹已创建。"),
+            Err(e) => println!("创建文件夹时出错: {}", e),
+        }
+    }
 }
---
+++ rustfile/target/debug/rustfile.d
@@ -0,0 +1,1 @@
+E:\T_EDB\EDB\erust_rs\rust\rustfile\target\debug\rustfile.exe: E:\T_EDB\EDB\erust_rs\rust\rustfile\src\main.rs
